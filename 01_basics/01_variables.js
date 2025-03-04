acd94a2439f6d38e1c94cec401bd020047768eb7
Muhammad Umair <arshadu452@gmail.com>
2025-03-04 12:29:10 +0000

    Variable conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+
+let var1 = 123
+
+// console.log('6' % '3');
+// console.log('true' -  1)
+
+// console.log(true + 1);   // ?
+// console.log(false + 1);  // ?
+// console.log(true + true); // ?
+// console.log("5" - true); // ?
+// console.log("5" + true); // ?
+
+// console.log(null + 1);   // ?
+// console.log(undefined + 1); // ?
+// console.log(null - 1);   // ?
+// console.log(undefined - 1); // ?
+// console.log("5" + null); // ?
+// console.log("5" - null); // ?
+
+
+// console.log([1, 2, 3] + [4, 5, 6]); // ?
+// console.log([] + {}); // ?
+console.log({} + []); // ?
+
+
+
