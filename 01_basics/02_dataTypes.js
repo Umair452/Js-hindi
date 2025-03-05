d109fd9480a115642d8eab38bb1150551cfa0777
Muhammad Umair <arshadu452@gmail.com>
2025-03-05 12:46:13 +0000

    data types basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//alert(3 + 3) 
+
+/* Primitive data types
+number => 2 raised to 53
+bigint => bigger then number
+string
+boolean
+null => itself a value but read as object
+undefined =>
+symbols => used for uniqueness */
+
