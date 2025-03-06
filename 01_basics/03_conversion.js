9f4f0c8df032eaddcd651a6b5c191fca7543a9c0
Muhammad Umair <arshadu452@gmail.com>
2025-03-06 11:06:17 +0000

    conversions of data types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,36 @@
+let age = undefined
+
+// console.log(typeof age);
+
+let valueInNumber = Number(age)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//String '33' is easily converted into number
+//String '33abc' is converted but value shows NAN
+//Null is 0
+//true / false is 0 / 1
+//undefined is NAN
+
+let isLoggedIn = undefined
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+//STring to boolean is always true except empty string which is false
+//Null is false 
+//Undefined is also false
+
+let myNumber = 1234
+let myNull = null
+let myUndefined = undefined
+let myArray = [1,2,3,4]
+// console.table([String(myNumber), String(myNull), String(myUndefined), String(myArray)])
+// console.log(typeof String(myArray));
+
+// console.log('Converting array to number' + Number(myArray)); //NAN
+
