d270e45ef1f3fb6866f7bd4ce64436adb1970ab3
Muhammad Umair <arshadu452@gmail.com>
2025-03-09 13:56:51 +0000

    basics of operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,9 @@
+console.log(2**3); //it is actually 2 raised to power 3
+console.log('1' + 2 + 2); //it is 122
+console.log(1 + 2 + '2'); //it is 32
+
+// console.log(3 + 2 - 1 * 2 / 2 % 2);
+
+
+console.log(+true); //it gives 1 as it is conversion example
+
